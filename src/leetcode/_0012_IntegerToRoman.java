4a2a20d7ffffdd84edcacd77f6cbcad07d484b74
Zhicheng Zhang <zzc-tongji@users.noreply.github.com>
2020-05-30 00:23:08 -0400

    commit

--- src/leetcode/_0012_IntegerToRoman.java
+++ src/leetcode/_0012_IntegerToRoman.java
@@ -67,12 +67,22 @@ public class _0012_IntegerToRoman {
   }
 
   public String intToRoman(int num) {
-    // TODO
-    return "";
-    /*
-     * time: O(1)
-     *
-     * space: O(1)
-     */
+    String[] strs = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+    int[] numbers = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+    StringBuilder sb = new StringBuilder();
+    int i = 0;
+    while (num > 0) {
+      while (num >= numbers[i]) {
+        sb.append(strs[i]);
+        num -= numbers[i];
+      }
+      i++;
+    }
+    return sb.toString();
   }
+  /*
+   * time: O(n)
+   *
+   * space: O(1)
+   */
 }
---
+++ src/techbow/_0009_DivideTwoInteger.java
@@ -0,0 +1,17 @@
+package techbow;
+
+public class _0009_DivideTwoInteger {
+  public static void main(String[] args) {
+    System.out.println("Hello world.");
+  }
+
+  public static int divide(int dividend, int divisor) {
+    // TODO
+    return 0;
+    /*
+     * time: O(1)
+     *
+     * space: O(1)
+     */
+  }
+}
