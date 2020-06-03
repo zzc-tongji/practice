da9ed60b9fb6a58bf798f373508372bfeab9f1cb
Zhicheng Zhang <zzc-tongji@users.noreply.github.com>
2020-06-03 15:09:43 -0400

    commit

--- src/techbow/_0003_mydatastructure/linkedlist/MyLinkedListNode.java
+++ src/techbow/_0003_mydatastructure/linkedlist/MyLinkedListNode.java
@@ -5,6 +5,10 @@ public class MyLinkedListNode<E> {
   public MyLinkedListNode<E> previous;
   public MyLinkedListNode<E> next;
 
+  public MyLinkedListNode() {
+    this(null);
+  }
+
   public MyLinkedListNode(E v) {
     value = v;
     previous = null;
